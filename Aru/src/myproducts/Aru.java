27ffa5574dd90c436a16fd0866737c99a41ee470
czako <czako@iit435.iit.uni-miskolc.hu>
2018-03-28 12:45:41 +0200

    aru

---
+++ Aru/src/myproducts/Aru.java
@@ -0,0 +1,41 @@
+package myproducts;
+
+public class Aru {
+    private String nev;
+    private int netto_ar;
+    private int afakulcs;
+    
+    
+	public Aru(String nev, int netto_ar, int afakulcs) {
+		this.nev = nev;
+		this.netto_ar = netto_ar;
+		this.afakulcs = afakulcs;
+	}
+    
+	public int brutto_ar (){
+	return (int) (netto_ar * (1 + afakulcs / 100.0));
+	}
+
+	@Override
+	public String toString() {
+		return "Aru [nev=" + nev + ", brutto_ar()=" + brutto_ar() + "]";
+	}
+   
+	public void nettoarnovelese(int szazalek){
+		netto_ar += (int) (netto_ar * (1 + szazalek / 100.0));
+	}
+	
+	public int osszehasonlitas(Aru masodikaru) {
+		if (brutto_ar() > masodikaru.brutto_ar() ) {
+			return 1;
+		}
+			
+		else if (brutto_ar()< masodikaru.brutto_ar() ){
+	    	return -1;
+		} else {
+			return 0;
+		
+		}
+		
+	}
+}
---
+++ Aru/src/myproducts/Kenyer.java
@@ -0,0 +1,12 @@
+package myproducts;
+
+public class Kenyer extends Aru {
+  private double mennyiseg;
+
+public Kenyer(String nev, int netto_ar, int afakulcs, double mennyiseg) {
+	super(nev, netto_ar, afakulcs);
+	this.mennyiseg = mennyiseg;
+}
+  
+  
+}
