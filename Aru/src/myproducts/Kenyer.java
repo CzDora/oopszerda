029126c6306bf2caaab9275c8c3ce6342af96ed4
czako <czako@iit435.iit.uni-miskolc.hu>
2018-04-04 12:59:54 +0200

    aru 2

--- Aru/src/myproducts/Kenyer.java
+++ Aru/src/myproducts/Kenyer.java
@@ -7,6 +7,25 @@ public Kenyer(String nev, int netto_ar, int afakulcs, double mennyiseg) {
 	super(nev, netto_ar, afakulcs);
 	this.mennyiseg = mennyiseg;
 }
+
+@Override
+public String toString() {
+	return "Kenyer [" + super.toString()
+	+ ", egysegar: "
+	+ brutto_ar() / mennyiseg   
+	+ "]";
+}
   
-  
+  public double getMennyiseg() {
+	  return mennyiseg;
+	
+}
+  public double egysegar() {
+	  return brutto_ar() / mennyiseg;
+	
+}
+  public static boolean elso_kenyer_nagyobb_egysegaru(Kenyer elso_kenyer, Kenyer masodik_kenyer) {
+	  return (elso_kenyer.egysegar() > masodik_kenyer.egysegar());  
+	
+}
 }
